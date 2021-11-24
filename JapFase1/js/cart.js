a12b9972315935780eb245fb79e268acf9c3e339
Alvaro Rovira <88434665+AlvaroDiploide@users.noreply.github.com>
2021-11-23 23:17:03 -0300

    Add files via upload

--- JapFase1/js/cart.js
+++ JapFase1/js/cart.js
@@ -1,69 +1,98 @@
-var cartproducts = [ ];
+var cartproducts = [];
+var costoEnvio = 1.15;
+let envio = document.getElementsByName('pago');
 
-// Funcion para mostrar imágenes
-function showImagesGallery() {
+
+// Funcion para mostrar carrito y datos correspondientes
+function mostrarCart() {
     let pord = cartproducts.articles;
-    let htmlContentToAppend = "";
-    let subTotal= 0;
-    let iva = 0;
-    let suma = 0;
+    let addEndHTML = "";
 
+  //  if (cartproducts.articles === 0) {
+    //    document.getElementById("carrito").innerHTML ="";
+   // }
 
     for (let i = 0; i < pord.length; i++) {
         let cartItem = pord[i];
-        suma +=(cartItem.count) *(cartItem.unitCost);
-        subTotal +=(cartItem.count) / 1.22;
-        iva += subTotal * 22;
 
-        htmlContentToAppend += `
+        addEndHTML += `
 
           <tr>
                     <th scope="row-2"></th>
                     <td class="table__productos">
-                        <img src="${cartItem.src}"alt="#">
+                        <img src="${cartItem.src}" alt="#">
                         <h6 class="Title"> "${cartItem.name}"</h6>
                     </td>
                     <td class="table__precio">
-                        <p>${cartItem.currency} ${cartItem.unitCost}</p>
+                        <p>${cartItem.currency} <span class="datos1"> ${cartItem.unitCost}</span></p>
                     </td>
-                    <td class="table__cantidad"><input type="number" min="1" value="${cartItem.count}">
+                    <td class="table__cantidad"><input type="number" min="1" onchange="cuentas('${cartItem.currency}')" value="${cartItem.count}">
+                    <button class="delete btn btn-danger" onclick="eliminar(${i})">Eliminar</button>
                     </td>
-                    <td class="row-cols-md-2"  id="subT">
-                    </td>
-                    <td class="row-cols-md-2"  id="iva(22%)">
-                    </td>
-                    <td class="row-cols-md-2"  id="suma">
-                    </td>
-                     
-
-                     <td class="row-cols-md-1"  >
-                     <button class="delete btn btn-danger">Eliminar</button>
-                        <button class="btn btn-success">Comprar</button>
-                    </td>
+               
                 </tr>
         `
+    }
+    document.getElementById("carrito").innerHTML = addEndHTML;
+    cuentas();
+}
+ 
+function eliminar(i) {
+    cartproducts.articles.splice(i, 1);
+    mostrarCart();
+    cuentas();
+}
+
+
+// Función para cálculo de total y sub-total (recuadro inferior)
+function cuentas(moneda) {
+    let uniCost = document.getElementsByClassName("datos1"); // obtener datos dentro de span
+    let cantProductos = document.getElementsByTagName("input"); // obtener valores de input
+    let subtotal = 0;
+    let costo = 0;
+    let cantItem = 0;
+
+    for (let i = 0; i < uniCost.length; i++) {
+        
+        if (moneda === "USD") {
+    
+            costo =  parseFloat(uniCost[i].innerHTML) * parseFloat(cantProductos[i].value);
+        } else {
+            costo = parseFloat(uniCost[i].innerHTML) * parseFloat(cantProductos[i].value);
+        }
+        subtotal += costo;
+            cantItem += parseFloat(cantProductos[i].value);
+    }
+    costoEnvio =0;
+    //let ninguno = true;
+    for (let x=0; x< envio.length; x++){
+        if (envio[x].checked){
+            costoEnvio = cantProductos * parseFloat(envio[x].value);
+            //ninguno = false;
+        }
 
     }
-    document.getElementById("carrito").innerHTML = htmlContentToAppend;
-    document.getElementById("suma").innerHTML = suma
-    document.getElementById("subT").innerHTML = subTotal
-    document.getElementById("iva(22%)").innerHTML = iva
+    
+    document.getElementById("sumaSub").innerHTML = `Sub-Total: USD ${subtotal}`;
+    document.getElementById("sumaTotal").innerHTML = `Total: UYU ${Math.round(subtotal * 40)}`;
+    document.getElementById('alerta').innerHTML=(costoEnvio).toFixed(2);
 }
 
 
 document.addEventListener("DOMContentLoaded", function (e) {
-    getJSONData(CART_INFO_URL).then(function (cartPino) {
+    getJSONData(CART_TWO).then(function (cartPino) {
         if (cartPino.status === "ok") {
             cartproducts = cartPino.data;
-            console.log (cartproducts);
-
-
-
           
-          
-            //Muestro las categorías ordenadas
-            showImagesGallery();
+            //Muestrar resultados
+            mostrarCart();
         }
 
     });
+    for (let i=0; i< envio.length; i++){
+        envio[i].addEventListener('click',()=>{
+            cuentas();
+        })
+       
+    }
 });
--- JapFase1/js/login.js
+++ JapFase1/js/login.js
@@ -18,7 +18,19 @@ function redireccionar (){
     }
 }
 
-// Funcion mostrar menu
+// funcion del switch Theme
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
 
 
 
